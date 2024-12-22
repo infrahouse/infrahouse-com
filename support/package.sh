2ac47d51766d3f368526af2369d804c07a370c40
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2024-12-22 12:47:10 -0800

    Autogenerate package version

--- support/package.sh
+++ support/package.sh
@@ -2,6 +2,18 @@
 
 set -eux
 
+
+function generate_changelog() {
+  cat > debian/changelog << EOF
+infrahouse-com (0.2.0-1build$(date +%s)) jammy; urgency=medium
+  * commit event. see changes history in git log
+
+ -- InfraHouse Packager <package@infrahouse.com>  $(date)
+
+EOF
+}
+generate_changelog
+
 upstream_version=$(head -1 debian/changelog | awk '{ print $2 }' | sed -e 's/[()]//g' | awk -F- '{ print $1 }')
 pkg_name="infrahouse-com"
 TMPDIR=$(mktemp -d)
